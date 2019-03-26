8e4168373762b3d1ad363f0fbd3ff327fc5aaa40
adamgparsons <adamgeorgeparsons@gmail.com>
2019-03-26 07:09:14 +0000

    update h1 with themeGet

--- src/components/heading1.js
+++ src/components/heading1.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 import { themeGet } from "styled-system";
 
 const Heading1 = styled.h1`
-  color: ${props => props.color || props.theme.colors.heritageBlue};
-  font-size: ${props => props.color || props.theme.fontSizes[4]};
-  line-height: ${props => props.color || props.theme.lineHeights[4]};
-  margin-bottom: ${props => props.color || props.theme.space[7]};
-  /* font-weight: ${themeGet("fontWeights.extraBold")}; */
+  color: ${themeGet("colors.heritageBlue")};
+  font-size: ${themeGet("fontSizes[4]")};
+  line-height: ${themeGet("lineHeights[4]")};
+  margin-bottom: ${themeGet("space[7]")};
+  font-weight: ${themeGet("fontWeights.extraBold")};
 `;
 
 export default Heading1;
